124fc7d03b9d70d7f36a8db0de7bfd891bbfa810
DebinAl <99970804+DebinAl@users.noreply.github.com>
2025-05-21 20:28:30 +0000

    feat!: Implement data acquisition via Kaggle API added download_dataset module to download dataset from the provided dataset identifier

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import prepare_data
+
+def main():
+    prepare_data.download_dataset('gabrielramos87/an-online-shop-business', 
+                                  'e-commerce.csv')
+
+if __name__ == "__main__":
+    main()
---
+++ src/prepare_data.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import kaggle
+
+def download_dataset(source: str, rename: str = None):
+    """ Downloads a dataset from Kaggle using the given source identifier.
+
+    Args:
+        source (str): The Kaggle dataset identifier `(e.g., 'user/dataset-name').`
+        rename (str, optional): New filename to rename the downloaded CSV file. 
+                                If not provided, the original filename is kept.
+    """
+    
+    kaggle.api.authenticate()
+    kaggle.api.dataset_download_files(source, path='data/', unzip=True)
+    
+    # Rename file
+    if rename != None:
+        for file in Path('data').glob('*.csv'):
+            if file.name != 'e-commerce.csv':
+                file.rename('data/e-commerce.csv')
+                break
+    
+    
+if __name__ == '__main__':
+    download_dataset('gabrielramos87/an-online-shop-business', 
+                     'e-commerce.csv')
